11c7c96b6a8252a15280108a98f1626a2b044e15
astephens-fsd <astephens.pm@icloud.com>
2021-09-10 16:05:39 -0700

    media querie @ 500 px

--- index.js
+++ index.js
@@ -243,8 +243,13 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array, years) {
+  const newArray = [];
+  for(let i = 0; i< array.length; i++){
+    if(array[i].years >= 1900 && array[i.years <= 2000]{
+      newArray.push
+    }
+  }
 }
 
 
